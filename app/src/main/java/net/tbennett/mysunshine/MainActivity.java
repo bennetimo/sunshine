859e297f3cf0acf2e4d792b17916b9ea8c080b52
Tim Bennett <bennetimo@gmail.com>
2015-11-01 14:21:30 +0000

    add settings activity

--- app/src/main/java/net/tbennett/mysunshine/MainActivity.java
+++ app/src/main/java/net/tbennett/mysunshine/MainActivity.java
@@ -1,10 +1,12 @@
 package net.tbennett.mysunshine;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -44,9 +46,10 @@ public class MainActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            Intent settingsIntent = new Intent(this, SettingsActivity.class);
+            startActivity(settingsIntent);
             return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
 }
