eff56e5c3115e7ab1c9b5533caf28429524e8933
Tim Bennett <bennetimo@gmail.com>
2015-11-01 18:06:18 +0000

    add ability to share weather via an intent

--- app/src/main/java/net/tbennett/mysunshine/DetailActivityFragment.java
+++ app/src/main/java/net/tbennett/mysunshine/DetailActivityFragment.java
@@ -3,7 +3,12 @@ package net.tbennett.mysunshine;
 import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.widget.ShareActionProvider;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -15,22 +20,58 @@ import org.w3c.dom.Text;
  */
 public class DetailActivityFragment extends Fragment {
 
+    private ShareActionProvider mShareActionProvider;
+    private String forecast;
+
     public DetailActivityFragment() {
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        setHasOptionsMenu(true);
+
         Intent intent = getActivity().getIntent();
 
         View rootView = inflater.inflate(R.layout.fragment_detail, container, false);
 
         if(intent != null && intent.hasExtra(Intent.EXTRA_TEXT)){
-            String forecast = intent.getStringExtra(Intent.EXTRA_TEXT);
+            forecast = intent.getStringExtra(Intent.EXTRA_TEXT);
             TextView detailText = (TextView) rootView.findViewById(R.id.detail_text);
             detailText.setText(forecast);
         }
 
         return rootView;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        //Locate menu item with ShareActionProvider
+        MenuItem item = menu.findItem(R.id.menu_item_share);
+
+        //Fetch and store ShareActionProvider
+        mShareActionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(item);
+
+        setupShareIntent(forecast);
+
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+
+    private void setupShareIntent(String forecast){
+
+        Intent sendIntent = new Intent();
+        sendIntent.setAction(Intent.ACTION_SEND);
+        sendIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
+        String shareString = getString(R.string.share_pre_text) + "\n" + forecast + " " + getString(R.string.share_append_text);
+        sendIntent.putExtra(Intent.EXTRA_TEXT, shareString);
+        sendIntent.setType("text/plain");
+        setShareIntent(sendIntent);
+    }
+
+    // Call to update the share intent
+    private void setShareIntent(Intent shareIntent) {
+        if (mShareActionProvider != null) {
+            mShareActionProvider.setShareIntent(shareIntent);
+        }
     }
 }
